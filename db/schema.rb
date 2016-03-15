cd26eef20a1a905722fd7439ca23c1a74fba776a
foxiae <xapatheiax@gmail.com>
2016-03-14 21:17:08 -0400

    Added User controller/updated DB for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220194300) do
+ActiveRecord::Schema.define(version: 20160314195122) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
   create_table "workout_sessions", force: :cascade do |t|
     t.string   "workout_focus"
