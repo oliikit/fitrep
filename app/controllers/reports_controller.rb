5ac572b88c822e592583f4e6d2de394ca7f14653
foxiae <xapatheiax@gmail.com>
2016-04-02 22:44:13 -0400

    generated controller for reports

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,12 @@
+class ReportsController < ApplicationController
+
+  def index
+    @workout_sessions = WorkoutSession.all
+  end
+
+  def show
+    @workout_session = WorkoutSession.find(params[:id])
+    @exercises = @workout_session.exercises
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/new'
 
   resources :workout_sessions
+  resources :reports
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
