0ed287758b1d6d7600a20f6d43a0ce7d6a91a63e
foxiae <xapatheiax@gmail.com>
2016-04-10 14:10:29 -0400

    include report on WS index

--- app/helpers/workout_sessions_helper.rb
+++ app/helpers/workout_sessions_helper.rb
@@ -1,21 +1,13 @@
 module WorkoutSessionsHelper
 
-  def time_data
-    (1.week.ago.to_date..Date.today).map do |date|
-      {
-        created_at: date,
-        workout_time: WorkoutSession.where("date(created_at) = ?", date).sum(:workout_time)
-      }
-    end
+  def last_workout_focus
+    @last = WorkoutSession.select("workout_focus, id").last
+    @last.workout_focus
   end
 
-  def link_to_add_fields(name, f, association)
-    new_object = f.object.send(association).klass.new
-    id = new_object.object_id
-    fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render(association.to_s.singularize + "_fields", f: builder)
-    end
-    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+  def average_workout_time
+    @avg_time = WorkoutSession.average(:workout_time)
+    @avg_time.round
   end
 
 end
