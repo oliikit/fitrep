34b74343187d0aea41b23cec0e51713e2cb2a729
foxiae <xapatheiax@gmail.com>
2016-04-10 11:05:26 -0400

    add news

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,13 @@
 class StaticPagesController < ApplicationController
   layout "home"
+  layout "application"
 
   def home
     render :layout => "home"
+  end
+
+  def news
+    render :layout => "application"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'users/new'
+
   root 'static_pages#home'
+  get 'news' => 'static_pages#news'
+
   resources :workout_sessions
   resources :reports
 end
