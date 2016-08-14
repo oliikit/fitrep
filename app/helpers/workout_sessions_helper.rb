dd9b260e353487728df8a2651346b229282b9be1
foxiae <olivia@staq.com>
2016-08-14 10:43:28 -0400

    add save guard for nil

--- app/helpers/workout_sessions_helper.rb
+++ app/helpers/workout_sessions_helper.rb
@@ -2,17 +2,24 @@ module WorkoutSessionsHelper
 
   def last_workout_focus
     @last = WorkoutSession.select("workout_focus, id").last
-    @last.workout_focus
+    if @last.nil?
+      "You have not logged any workouts."
+    else
+      @last.workout_focus
+    end
   end
 
   def average_workout_time
     @avg_time = WorkoutSession.average(:workout_time)
-    @avg_time.round
   end
 
   def last_workout_date
-    @last = WorkoutSession.select("completed_date, id").last
-    @last.completed_date.to_s(:long)
+    @last = WorkoutSession.select("completed_date").last
+    if @last.nil?
+      "You have not logged any workouts."
+    else
+      @last.completed_date.to_s(:long)
+    end
   end
 
 end
