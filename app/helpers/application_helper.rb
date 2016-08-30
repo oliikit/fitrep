8709abb453ec5dcbaa2af5024a7fd59eab0a1776
foxiae <olivia@staq.com>
2016-08-29 21:55:33 -0400

    cleaned up dashboard and headers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def active_class(link_path)
+    current_page?(link_path) ? "active" : ""
+  end
+
+  def link_fa_to(icon_name, text, link)
+    link_to content_tag(:i, text, :class => "fa fa-#{icon_name}"), link
+  end
+
 end
